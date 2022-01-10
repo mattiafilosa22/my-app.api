46f7d661b902903550055f15dbaa2b6ebc774195
mattiafilosa <mattia.filosa@hibo.it>
2022-01-10 16:54:00 +0100

    Fix

--- controllers/image.js
+++ controllers/image.js
@@ -5,7 +5,7 @@ const app = new Clarifai.App({
 });
 const handleApiCall = (req, res) => {
     app.models
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .predict(clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => {
             res.json(data)
         })
